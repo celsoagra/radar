3ee7586a7e9f904fbfed044cfd5b7b25f62dd3b0
celsoagra <celsoagra@gmail.com>
2017-01-05 01:07:17 -0300

    added new systems

--- js/data.js
+++ js/data.js
@@ -19,8 +19,44 @@ var data = [
     },
     {
         "name" : "Artifactory JFrog",
-        "version" : "2.2.2",
+        "version" : "4.15.0",
         "description" : "JFrog provides open source communities with solutions to automate software package management.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "#",
+        "position" : "right: 20%; bottom: 40%;"
+    },
+    {
+        "name" : "Gitlab",
+        "version" : "8.15",
+        "description" : "Provides Git repository management, code reviews, issue tracking, activity feeds and wikis. GitLab itself is also free software.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "#",
+        "position" : "right: 30%; bottom: 40%;"
+    },
+    {
+        "name" : "Redmine",
+        "version" : "3.3.1",
+        "description" : "is a free and open source, web-based project management and issue tracking tool. It allows users to manage multiple projects and associated subprojects.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "#",
+        "position" : "right: 40%; bottom: 40%;"
+    },
+    {
+        "name" : "Keycloak",
+        "version" : "2.5.0.Final",
+        "description" : "Add authentication to applications and secure services with minimum fuss. No need to deal with storing users or authenticating users. It's all available out of the box.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "#",
+        "position" : "right: 35%; bottom: 45%;"
+    },
+    {
+        "name" : "Apiman",
+        "version" : "1.2.8.Final",
+        "description" : "The apiman project brings an open source development methodology to API Management, coupling a rich API design & configuration layer with a blazingly fast runtime.",
         "responsible" : "",
         "responsible_email" : "",
         "link" : "#",
