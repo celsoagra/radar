bd49ba1b01f62abb68d555b37fd9740f2a3b9559
celsoagra <celsoagra@gmail.com>
2017-01-09 23:54:40 -0300

    added new elements

--- js/data.js
+++ js/data.js
@@ -22,7 +22,7 @@ var data = [
     {
         "name" : "Minimum Viable Product (MVP)",
         "version" : "",
-        "description" : "",
+        "description" : "Is a product with just enough features to gather validated learning about the product and its continued development. Gathering insights from an MVP is often less expensive than developing a product with more features, which increase costs and risk if the product fails, for example, due to incorrect assumptions.",
         "responsible" : "",
         "responsible_email" : "",
         "link" : "",
@@ -70,42 +70,12 @@ var data = [
         "type" : "tool"
     },
     {
-        "name" : "Keycloak",
-        "version" : "2.5.0.Final",
-        "description" : "Add authentication to applications and secure services with minimum fuss. No need to deal with storing users or authenticating users. It's all available out of the box.",
-        "responsible" : "",
-        "responsible_email" : "",
-        "link" : "https://www.keycloak.org",
-        "position" : "top: 45%; left: 70%;",
-        "type" : "tool"
-    },
-    {
-        "name" : "Apiman",
-        "version" : "1.2.8.Final",
-        "description" : "The apiman project brings an open source development methodology to API Management, coupling a rich API design & configuration layer with a blazingly fast runtime.",
-        "responsible" : "",
-        "responsible_email" : "",
-        "link" : "www.apiman.io",
-        "position" : "top: 40%; left: 70%;",
-        "type" : "tool"
-    },
-    {
-        "name" : "Docker",
-        "version" : "1.12.8",
-        "description" : "",
-        "responsible" : "",
-        "responsible_email" : "",
-        "link" : "",
-        "position" : "top: 55%; left: 45%;",
-        "type" : "platform"
-    },
-    {
         "name" : "Angular2",
-        "version" : "x",
-        "description" : "",
+        "version" : "",
+        "description" : "Angular is a development platform for building mobile and desktop web applications.",
         "responsible" : "",
         "responsible_email" : "",
-        "link" : "",
+        "link" : "https://angular.io/",
         "position" : "top: 67%; left: 63%;",
         "type" : "framework"
     },
@@ -138,5 +108,185 @@ var data = [
         "link" : "",
         "position" : "top: 42%; left: 40%;",
         "type" : "technique"
+    },
+    {
+        "name" : "HTTP Strict Transport Security (HSTS)",
+        "version" : "",
+        "description" : "With HSTS, the server sends a header that informs the browser that it should only use HTTPS to access the website. Browser support is now widespread enough that this easy-to-implement feature should be added to any site using HTTPS.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 55%; left: 40%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Linux security modules",
+        "version" : "",
+        "description" : "With SELinux or AppArmor included by default in most Linux distributions, and with more comprehensive tools such as Grsecurity readily available, These tools help teams assess questions about who has access to what resources on shared hosts, including contained services. This conservative approach to access management will help teams build security into their SDLC processes.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 58%; left: 44%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Apache Mesos",
+        "version" : "",
+        "description" : "Used to manage cluster resources for highly distributed systems. Mesos abstracts out underlying computing resources such as CPU and storage, aiming to provide efficient utilization while maintaining isolation. Mesos includes Chronos for distributed and fault-tolerant execution of scheduled jobs, and Marathon for orchestrating long-running processes in containers.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 55%; left: 29%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Keycloak",
+        "version" : "2.5.0.Final",
+        "description" : "Add authentication to applications and secure services with minimum fuss. No need to deal with storing users or authenticating users. It's all available out of the box.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "https://www.keycloak.org",
+        "position" : "top: 68%; left: 37%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Apiman",
+        "version" : "1.2.8.Final",
+        "description" : "The apiman project brings an open source development methodology to API Management, coupling a rich API design & configuration layer with a blazingly fast runtime.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "www.apiman.io",
+        "position" : "top: 63%; left: 34%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Docker",
+        "version" : "1.12.8",
+        "description" : "Docker is an open platform for developers and sysadmins to build, ship, and run distributed applications, whether on laptops, data center VMs, or the cloud.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "https://www.docker.com/",
+        "position" : "top: 52%; left: 45%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "JBoss Fuse",
+        "version" : "",
+        "description" : "Main Content Red Hat JBoss Fuse is an open source, lightweight and modular integration platform with a new-style Enterprise Service Bus (ESB) that supports integration beyond the data center. The capability to connect all enterprise assets and the ability to deploy JBoss Fuse in several different configurations advances intelligent integration to all facets of your business – on premise or in the Cloud.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 67%; left: 43%;",
+        "type" : "platform"
+    },
+    {
+        "name" : ".NET Core",
+        "version" : "",
+        "description" : "Is an open source modular product for creating applications that can be easily deployed in Windows, macOS and Linux. .NET Core makes it possible to build cross-platform web applications using ASP.NET Core with a set of tools, libraries and frameworks—another choice for microservices architecture. There are Docker images based on both Linux and Windows (Nano Server) with .NET Core that simplify applying a microservice architecture.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 64%; left: 18%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Electron",
+        "version" : "",
+        "description" : "Electron is a solid framework for building native desktop clients using web technologies such as HTML, CSS and JavaScript. Teams can leverage their web know-how to deliver polished cross-platform desktop clients without spending time learning another set of technologies.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "http://electron.atom.io/",
+        "position" : "top: 71%; left: 23%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Overambitious API gateway",
+        "version" : "",
+        "description" : " API gateways can provide utility in dealing with some generic concerns — for example, authentication and rate-limiting—but any domain smarts such as data transformation or rule processing should live in applications or services where they can be controlled by product teams working closely with the domains they support.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 77%; left: 14%;",
+        "type" : "platform"
+    },
+    {
+        "name" : "Graphite, Kibana or Grafana",
+        "version" : "",
+        "description" : "Teams need increasingly sophisticated monitoring. Simply looking a graphs of disk usage and CPU utilization is not sufficient anymore, and many teams collect application and business-specific metrics using tools such a Graphite and Kibana. Grafana makes it easy to create useful and elegant dashboards for data from a number of sources. A particularly useful feature allows timescales of different graphs to be synchronized, which helps with spotting correlations in the underlying data.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 40%; left: 52%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "HashiCorp Vault",
+        "version" : "",
+        "description" : "Having a way to securely manage secrets is increasingly becoming a huge project issue. The old practice of keeping secrets in a file or in environment variables is becoming hard to manage, especially in environments with multiple applications and large numbers of microservices. HashiCorp Vault addresses the problem by providing mechanisms for securely accessing secrets through a unified interface.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "https://github.com/hashicorp/vault",
+        "position" : "top: 41%; left: 67%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "JSONassert",
+        "version" : "",
+        "description" : "Writing tests in Java for JSON can be laborious. JSONassert is a small library to help write smaller tests dealing with JSON by simplifying assertions and providing better error messages.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 45%; left: 72%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "Talisman",
+        "version" : "",
+        "description" : "Is a prepush hook for Git that scans commits for secrets matching predefined patterns.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 34%; left: 64%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "Terraform, Puppet, Chef or Ansible",
+        "version" : "",
+        "description" : "Manage cloud infrastructure by writing declarative definitions. The configuration of the servers instantiated by Terraform is usually left to tools like Puppet, Chef or Ansible.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 31%; left: 53%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "ScreenHero or tmate",
+        "version" : "",
+        "description" : "Graphical screen-sharing solutions.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 25%; left: 55%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "Bottled Water",
+        "version" : "",
+        "description" : "Is a welcome addition to this field, converting changes in PostgreSQL’s write-ahead log into Kafka events. One downside of this approach, however, is that you are tied to low-level database events rather than the higher-level business events we recommend as the foundation for an eventoriented architecture.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "",
+        "position" : "top: 27%; left: 74%;",
+        "type" : "tool"
+    },
+    {
+        "name" : "Vertx",
+        "version" : "",
+        "description" : "Eclipse Vert.x is a tool-kit for building reactive applications on the JVM.",
+        "responsible" : "",
+        "responsible_email" : "",
+        "link" : "http://vertx.io/",
+        "position" : "top: 60%; left: 82%;",
+        "type" : "framework",
     }
 ]
